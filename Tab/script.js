70ef5c18ac7164e5e2f87dea9955869bc8ac2d55
長井崚介 <ryooo@nagainoMacBook-Pro.local>
2021-08-04 18:36:32 +0900

    Tabファイルの作成

---
+++ Tab/script.js
@@ -0,0 +1,16 @@
+$(function () {
+  //tab1以外を非表示にする
+  $('#contents div[id != "tab1"]').hide();
+  //タブをクリック
+  $('a').click(function () {
+    //一度全てのコンテンツを非表示にする
+    $('#contents div').hide();
+    // href属性を取得
+    $($(this).attr('href')).show();
+    //現在のcurrentクラスを削除
+    $('.current').removeClass('current');
+    //選択されたタブ（自分自身）にcurrentクラスを追加
+    $(this).addClass('current');
+    return false;
+  });
+});
--- Viewer/script.js
+++ Viewer/script.js
@@ -1,8 +1,10 @@
 $(function () {
+  //サムネをクリック
   $('a').click(function () {
     //拡大画像のsrc属性に、選択したa要素のhref属性をいれる
     $('figure img').attr('src', $(this).attr('href'));
 
+    //a要素の機能を無効にする
     return false;
   });
 });
